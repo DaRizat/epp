6dea6adc8ed4eb14dd27795fe91088bf9556063a
Jack Bishop <jack.bishop@synctree.com>
2013-03-15 11:43:17 -0700

    adding config file

---
+++ lib/epp/config.rb
@@ -0,0 +1,14 @@
+module Epp
+  module Config
+
+    @@config = YAML::load(File.open("#{Dir.pwd}/config/epp.yml"))
+
+    def self.registry_config
+      @@config[Rails.env]
+    end
+
+    def self.domain_config
+      @@config['domain_states']
+    end
+  end
+end
